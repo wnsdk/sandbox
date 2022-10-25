451fe1c72342332eed22a3c182bb7a5539ea52b3
wnsdk <jn307742@gmail.com>
2022-10-25 17:39:39 +0900

    first commit

---
+++ src/main/java/com/wnsdk/board/service/BoardService.java
@@ -0,0 +1,18 @@
+package com.wnsdk.board.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.wnsdk.util.PageNavigation;
+import com.wnsdk.board.model.BoardDto;
+
+public interface BoardService {
+	
+	int writeArticle(BoardDto boardDto) throws Exception;
+	List<BoardDto> listArticle(Map<String, String> map) throws Exception;
+	PageNavigation makePageNavigation(Map<String, String> map) throws Exception;
+	BoardDto getArticle(int articleNo) throws Exception;
+	void updateHit(int articleNo) throws Exception;
+	void modifyArticle(BoardDto boardDto) throws Exception;
+	void deleteArticle(int articleNo) throws Exception;
+}
---
+++ src/main/java/com/wnsdk/interceptor/ConfirmInterceptor.java
@@ -0,0 +1,34 @@
+package com.wnsdk.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class ConfirmInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
