700a603a179726b1aea5cdf8027eb14c65c478b0
최준아 <jn307742@gmail.com>
2023-03-15 09:59:37 +0900

    feat: [spring-core] 스프링 핵심 원리 이해1 - 예제 만들기

---
+++ spring-core/src/test/java/hello/springcore/order/OrderServiceTest.java
@@ -0,0 +1,28 @@
+package hello.springcore.order;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import hello.springcore.member.Grade;
+import hello.springcore.member.Member;
+import hello.springcore.member.MemberService;
+import hello.springcore.member.MemberServiceImpl;
+
+class OrderServiceTest {
+
+	MemberService memberService = new MemberServiceImpl();
+	OrderService orderService = new OrderServiceImpl();
+
+	@Test
+	void createOrder() {
+		Long memberId = 1L;
+		Member member = new Member(memberId, "memberA", Grade.VIP);
+		memberService.join(member);
+
+		Order order = orderService.createOrder(memberId, "itemA", 10000);
+		Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+	}
+
+}
