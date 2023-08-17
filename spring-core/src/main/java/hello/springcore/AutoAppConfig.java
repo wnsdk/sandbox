dd98e80c536ac950e81c3fc87ef5b0715992aedb
wnsdk <jn307742@gmail.com>
2023-08-17 17:21:45 +0900

    [spring-core] 컴포넌트 스캔 / 탐색 위치와 기본 스캔 대상

--- spring-core/src/main/java/hello/springcore/AutoAppConfig.java
+++ spring-core/src/main/java/hello/springcore/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.springcore.member",
+//        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
