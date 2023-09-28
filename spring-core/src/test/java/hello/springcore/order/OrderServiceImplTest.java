bcf072b68dbe55868b900c05cfded13d87fadbdf
wnsdk <jn307742@gmail.com>
2023-09-28 13:33:46 +0900

    [spring-core] 의존관계 자동 주입 / 생성자 주입을 선택해라!

---
+++ spring-core/src/test/java/hello/springcore/order/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package hello.springcore.order;
+
+import hello.springcore.discount.FixDiscountPolicy;
+import hello.springcore.member.Grade;
+import hello.springcore.member.Member;
+import hello.springcore.member.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() {
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
